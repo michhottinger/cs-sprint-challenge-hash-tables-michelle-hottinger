b9155e553caba1b4b9fe5c6a4c4f5b225983c6a9
michhottinger <michhottinger@users.noreply.github.com>
2020-07-10 08:40:42 -0700

    ex1 completed and test passed

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,23 @@
+
+
+
+
 def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
     # Your code here
-
-    return None
+    hash = {}
+    for i in weights:#put weights values into hash table if not already there
+        if i not in hash:
+            hash[i] = limit - i
+        else:
+            hash[f'{i + 1}'] = limit - i
+     
+    for i in hash:#loop through i and j in hash to see if they sum to limit
+        for j in hash:
+            if hash[i] + hash[j] == limit and i != j:
+                #return the correct format which is index keys j and i
+                return list(hash.keys()).index(j), list(hash.keys()).index(i)
+    else:
+        return None
