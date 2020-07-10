fc3471d05b53fce3317132790a05646856a22acb
michhottinger <michhottinger@users.noreply.github.com>
2020-07-10 09:13:12 -0700

    completed ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,23 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
-
+    hash = {}
+    route = []
+    
+    for ticket in tickets:
+        if ticket.source not in hash:
+            hash[ticket.source] = ticket.destination #key, value pair with source and dest
+    
+    cur = "NONE" #start at begining
+    while True:
+        for ticket in hash:
+            if ticket == cur:
+                route.append(hash[ticket])
+                cur = hash[ticket]#move the pointer along the linked list
+        
+        print(cur)
+        if cur == "NONE":
+            break
+            
     return route
+
